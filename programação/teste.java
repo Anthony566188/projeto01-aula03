0061b7283e6dfb3ce133e5687d8d211a744879b6
Flávio <coelhoflavio785@gmail.com>
2025-03-15 11:50:42 -0300

    adicionando programação

---
+++ programação/teste.java
@@ -0,0 +1,13 @@
+package ddd;
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+        //imprimir
+        system.out.println("Hello, world!");
+
+        string nome = "Anthony";
+        //operador + significa concatenação
+        system.out.println("olá, " + nome);
+    }
+}
